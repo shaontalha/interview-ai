b3c37ff42c0fc3a1b501e76c50a55465f6f5220f
Talha Nadim Shaon <58784845+shaontalha@users.noreply.github.com>
2026-09-22 09:57:42 +0600

    Refactor database connection handling in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,29 @@
-const connectToDB = require("./config/database");
+const express = require("express")
+const app = express()
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const connectToDB = require("./config/database")
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "https://interview-ai-zeta-silk.vercel.app",
+    credentials: true
+}))
 
 app.use(async (req, res, next) => {
     try {
-        await connectToDB();
-        next();
+        await connectToDB()
+        next()
     } catch (err) {
-        res.status(503).json({ message: "Database connection failed" });
+        res.status(503).json({ message: "Database connection failed" })
     }
-});
+})
+
+const authRoutes = require("./routes/auth.routes")
+const interviewRouter = require('./routes/interview.routes')
+
+app.use("/api/auth", authRoutes)
+app.use("/api/interview", interviewRouter)
+
+module.exports = app
